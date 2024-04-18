2eb1cffa7f997fd4ef0a2fbf9e5ac51d1ae2837f
Mausham Gurung <maushamgurung999@Maushams-Air.lan>
2024-04-18 17:34:29 +0100

    creating player board for players

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,12 +4,24 @@ let boardWidth = 500;
 let boardHeight = 500;
 let context;
 
-//player board
+//player character
+let playerWidth = 80;
+let playerHeight = 10;
 
+let player = {
+    x : boardWidth/2 - playerWidth/2,
+    y : boardHeight - playerHeight - 5,
+    width : playerWidth,
+    height : playerHeight
+}
 window.onload = function() {
     board = document.getElementById("board")
     board.height = boardHeight
     board.width = boardWidth
     //for drawing on the board
     context = board.getContext("2d");
+
+    //player character
+    context.fillStyle = "Gold";
+    context.fillRect(player.x, player.y, player.width, player.height);
 }
