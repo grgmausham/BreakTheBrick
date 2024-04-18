9adec5873832392b4cd3e9bd2f234358f155aa5e
Mausham Gurung <maushamgurung999@Maushams-Air.lan>
2024-04-18 18:00:15 +0100

    making player board movabale on leftarrow and right arrow command

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -7,12 +7,15 @@ let context;
 //player character
 let playerWidth = 80;
 let playerHeight = 10;
+let playerVelocityX = 10;
+
 
 let player = {
     x : boardWidth/2 - playerWidth/2,
     y : boardHeight - playerHeight - 5,
     width : playerWidth,
-    height : playerHeight
+    height : playerHeight,
+    velocityX : playerVelocityX
 }
 window.onload = function() {
     board = document.getElementById("board")
@@ -24,4 +27,22 @@ window.onload = function() {
     //player character
     context.fillStyle = "Gold";
     context.fillRect(player.x, player.y, player.width, player.height);
+
+    requestAnimationFrame(update);
+    document.addEventListener("keydown", movePlayer);
+}
+
+function update() {
+    requestAnimationFrame(update);
+    context.fillStyle = "Gold";
+    context.fillRect(player.x, player.y, player.width, player.height);
+}
+
+function movePlayer(e) {
+    if (e.code == "ArrowLeft") {
+        player.x -= player.velocityX;
+    }
+    else if (e.code == "ArrowRight") {
+        player.x += player.velocityX
+    }
 }
